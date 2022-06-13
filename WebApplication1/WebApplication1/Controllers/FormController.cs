61a1cc9504d852819f5f671a45dcfea9808d9804
André Fragas <66977555+AndreFragas@users.noreply.github.com>
2022-06-12 23:26:07 -0300

    Finalizado o Frontend

---
+++ WebApplication1/WebApplication1/Controllers/FormController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjetoPrisma5.Controllers
+{
+    [ApiController]
+    [Route("api/projeto")]
+    public class FormController : ControllerBase
+    {
+        [HttpPost]
+        public void teste(string email, string estado)
+        {
+            ManipularJson manipulador = new ManipularJson();
+            manipulador.ShowData(estado);
+        }
+    }
+}
